6c25ec3138460285002d25fff83666a79aa8fa95
Zubin Thampi ARC <zubin.thampi@gmail.com>
2017-09-26 21:11:36 -0400

    Completed V1 part 2

--- hw3/lake/lake.c
+++ hw3/lake/lake.c
@@ -248,8 +248,10 @@ void update_sim_values(double *un, double *uo, double *uc, double *pebbles, int 
 
   /* run a central finite differencing scheme to solve
    * the wave equation in 2D */
+#pragma omp parallel for firstprivate(j,idx) num_threads(nthreads) 
   for( i = 0; i < n; i++)
   {
+#pragma omp parallel for firstprivate(idx) num_threads(nthreads)
     for( j = 0; j < n; j++)
     {
       idx = j + i * n;
@@ -436,4 +438,5 @@ void print_heatmap(char *filename, double *u, int n, double h)
   
   fclose(fp);
 } 
+
 
